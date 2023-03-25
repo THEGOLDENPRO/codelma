5b7bab2f53660be22eda4367069f1607e14b586a
Dev Goldy <jassim7256@gmail.com>
2023-03-25 13:58:47 +0000

    add logger and removed ping command

--- bot.py
+++ bot.py
@@ -1,13 +1,28 @@
 import nextcord
+import logging as log
 from nextcord.ext import commands
 from decouple import config
+from devgoldyutils import Colours, LoggerAdapter, add_custom_handler
+
+codelma_logger = add_custom_handler(
+    log.getLogger(Colours.PINK_GREY.apply_to_string("CODELMA"))
+)
 
 bot = commands.Bot()
 
+# Events
+# -------------
+@bot.event
+async def on_ready():
+    codelma_logger.info(Colours.GREEN.apply_to_string("[We're ready!]"))
 
-@bot.slash_command() # Just a basic ping command I added to test to see if the bot was working. (Will remove later.)
-async def ping(interaction: nextcord.Interaction):
-    await interaction.send("Pong!", ephemeral=True)
+
+# Commands
+# -------------
+@bot.slash_command(description="⭐ Begin your journey.", guild_ids=[863416692083916820]) # Added my guild id for testing, add yours too if you would like to test this command.
+async def solve(interaction: nextcord.Interaction):
+    await interaction.send("work in progress...", ephemeral=True)
 
 
+codelma_logger.setLevel(log.DEBUG)
 bot.run(config("TOKEN", cast=str))
