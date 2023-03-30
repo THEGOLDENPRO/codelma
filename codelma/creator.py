34946bc9fb3ea16d0058fb57b6870a5b4e0f44ed
JoJoJux aka. J0J0HA <info@jojojux.de>
2023-03-30 22:58:52 +0200

    Added support for YouTube avatars

--- codelma/creator.py
+++ codelma/creator.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 import json
+import re
 import urllib.parse
 from devgoldyutils import LoggerAdapter
 
@@ -22,8 +23,22 @@ class Creator:
         self.name = data.get("name", self.id)
         self.link = data.get("social_link")
         if self.link:
-            self.icon = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON"\
-            f"&fallback_opts=TYPE,SIZE,URL&url={urllib.parse.quote_plus(self.link)}&size=256"
+            match_yt = re.match(
+                "https?\:\/\/(www\.)?youtube\.com/channel/(.+)", self.link or "."
+            )
+            if match_yt and match_yt.start != match_yt.end:
+                channel_id = (
+                    match_yt.string.split("youtube.com/channel", 1)[1]
+                    .split("?", 1)[0]
+                    .split("#", 1)[0]
+                )
+                self.icon = f"https://banner.yt/{channel_id}/avatar"
+                print(self.icon)
+            else:
+                self.icon = (
+                    "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON"
+                    f"&fallback_opts=TYPE,SIZE,URL&url={urllib.parse.quote_plus(self.link)}&size=256"
+                )
         else:
             self.link = "https://github.com/THEGOLDENPRO/codelma"
             self.icon = "https://github.com/THEGOLDENPRO/codelma/raw/master/assets/in_development.png"
