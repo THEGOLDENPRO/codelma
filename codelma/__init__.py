db7a4bd2bed44fe6acfd970a73b93e0a8f47a4f6
JoJoJux aka. J0J0HA <info@jojojux.de>
2023-03-30 21:29:30 +0200

    Added creator caching

--- codelma/__init__.py
+++ codelma/__init__.py
@@ -14,22 +14,25 @@ codelma_logger = add_custom_handler(
 codelma_logger.setLevel(getattr(log, config("LOG_LEVEL", default="INFO")))
 
 from .quiz import Quiz
+from .creator import Creator
 from .quiz_types import QuizTypes
 
-class Codelma():
+
+class Codelma:
     """The main class. Allows you to retrieve quizzes."""
+
     def __init__(self) -> None:
         self.logger = LoggerAdapter(codelma_logger, prefix="Codelma")
 
-        self.__quizzes:Dict[str, List[Tuple[dict, str|None, str]]] = {}
-        
+        self.__quizzes: Dict[str, List[Tuple[dict, str | None, str]]] = {}
+        self.__creators: Dict[str, Creator] = {}
+
         for type in QuizTypes:
             self.__quizzes[type.name.lower()] = []
 
         self.global_quiz_count = 0
 
         self.__load_quizzes()
-
 
     def get_quiz(self, type: QuizTypes | str) -> Quiz:
         """Returns a random quiz from that type."""
@@ -43,9 +46,11 @@ class Codelma():
         """Returns a list of quizzes from that type."""
         if isinstance(type, QuizTypes):
             type = type.name
-        
-        return [Quiz(quiz[0], quiz[1], quiz[2], quiz[3]) for quiz in self.__quizzes[type.lower()]]
-    
+
+        return [
+            Quiz(quiz[0], quiz[1], quiz[2], quiz[3])
+            for quiz in self.__quizzes[type.lower()]
+        ]
 
     # If you wondering what the underscore is for, these are just private methods that are used internally in this class and are not be used externally.
     # ------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -53,64 +58,84 @@ class Codelma():
         """Method that looks for all quizzes and loads/caches them within this class. Usually ran after bot is ready."""
         self.__clear_cache()
 
-        for author in os.listdir("./quizzes"):
+        # Caching creator metadata
+        for creator in os.listdir("./creators"):
+            creator_id = creator.removesuffix(".json")
+            self.logger.debug(
+                Colours.PINK_GREY.apply_to_string(
+                    f"Loading creator metatada {creator_id}..."
+                )
+            )
+            self.__creators[creator_id] = Creator(creator_id)
+
+        # Caching Quizzes
+        for creator in self.__creators.values():
 
             id_count = 0
             not_loaded_count = 0
-            for file in os.listdir(f"./quizzes/{author}"):
+            for file in os.listdir(f"./quizzes/{creator.id}"):
 
                 if os.path.splitext(file)[1] == ".json":
                     id_count += 1
-                    self.logger.debug(Colours.PINK_GREY.apply_to_string(f"Loading {author}/{id_count}..."))
+                    self.logger.debug(
+                        Colours.PINK_GREY.apply_to_string(
+                            f"Loading {creator.id}/{id_count}..."
+                        )
+                    )
 
-                    json_config:dict = json.load(open(f"./quizzes/{author}/{id_count}.json"))
-
+                    json_config: dict = json.load(
+                        open(f"./quizzes/{creator.id}/{id_count}.json")
+                    )
 
                     # If the 'omit_code' json key is not set to true, we will search for a python code snippet.
                     # -------------------------------------------------------------------------------------------
-                    python_code_snippet:str|None = None
+                    python_code_snippet: str | None = None
 
                     if json_config.get("omit_code", False) is False:
 
                         try:
-                            python_code_snippet:str = open(f"./quizzes/{author}/{id_count}.py", mode="r").read()
+                            python_code_snippet: str = open(
+                                f"./quizzes/{creator.id}/{id_count}.py", mode="r"
+                            ).read()
                         except FileNotFoundError as e:
                             self.logger.error(
-                                f"We got a FileNotFoundError when loading quiz '{author}/{id_count}'. Make sure 'omit_code' is set to true if you wish to not include code snippet.\n" + 
-                                f"Error >> {e}"
+                                f"We got a FileNotFoundError when loading quiz '{creator.id}/{id_count}'. Make sure 'omit_code' is set to true if you wish to not include code snippet.\n"
+                                + f"Error >> {e}"
                             )
-
 
                     # Appending the quiz dict and python code to the appropriate type list in cache.
                     # -------------------------------------------------------------------------------
-                    type:str|None = json_config.get("type")
+                    type: str | None = json_config.get("type")
 
                     if not type is None:
 
                         try:
 
                             self.__quizzes[type.lower()].append(
-                                (json_config, python_code_snippet, id_count, author)
+                                (json_config, python_code_snippet, id_count, creator)
                             )
 
                         except KeyError as e:
                             self.logger.error(
-                                f"We got a KeyError when loading quiz '{author}/{id_count}'. The type may be incorrectly typed or spelt, please check it.\n" + 
-                                f"Error >> {e}"
+                                f"We got a KeyError when loading quiz '{creator.id}/{id_count}'. The type may be incorrectly typed or spelt, please check it.\n"
+                                + f"Error >> {e}"
                             )
 
                     else:
                         not_loaded_count += 1
                         self.logger.error(
-                            f"The quiz '{author}/{id_count}' does not contain a type so it will NOT be loaded."
+                            f"The quiz '{creator.id}/{id_count}' does not contain a type so it will NOT be loaded."
                         )
 
-            self.global_quiz_count += (id_count - not_loaded_count)
-            self.logger.info(f"Loaded {Colours.ORANGE + str(id_count - not_loaded_count) + Colours.RESET_COLOUR} quizzes from '{author}'s.")
+            self.global_quiz_count += id_count - not_loaded_count
+            self.logger.info(
+                f"Loaded {Colours.ORANGE + str(id_count - not_loaded_count) + Colours.RESET_COLOUR} quizzes from '{creator.id}'."
+            )
 
     def __clear_cache(self) -> None:
         """Method that clears the cache."""
+        self.__creators = {}
         for type in self.__quizzes:
             self.__quizzes[type] = []
-        
+
         self.logger.debug("Cleared quizzes cache!")
