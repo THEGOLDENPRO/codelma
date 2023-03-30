05477dc7a577c630cd24f8a7560969da447218b4
JoJoJux aka. J0J0HA <info@jojojux.de>
2023-03-30 21:11:30 +0200

    Added Creator class

--- codelma/creator.py
+++ codelma/creator.py
@@ -1,28 +1,31 @@
 from __future__ import annotations
 
-from typing import List
 from dataclasses import dataclass, field
-from devgoldyutils import DictDataclass, LoggerAdapter
 import json
+import urllib.parse
+from devgoldyutils import DictDataclass, LoggerAdapter
 
 
 @dataclass
 class Creator(DictDataclass):
     data: dict = field(repr=False)
-    
+
     id: str
     name: str = field(init=False)
-    social_link: str = field(init=False, repr=False)
-    icon_url: str = field(init=False, repr=False)
+    link: str = field(init=False, repr=False)
+    icon: str = field(init=False, repr=False)
 
     def __post_init__(self):
-        self.logger = LoggerAdapter(codelma_logger, prefix=f"Creator")
+        self.logger = LoggerAdapter(codelma_logger, prefix="Creator")
         super().__post_init__()
 
-        with open(f"creators/{self.id}", "r") as file:
+        with open(f"creators/{self.id}", "r", encoding="utf-8") as file:
             data = json.load(file)
-        
-        
+
+        self.name = data.get("name", self.id)
+        self.link = data.get("link", "https://google.com")
+        self.icon = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON"\
+            f"&fallback_opts=TYPE,SIZE,URL&url={urllib.parse.quote_plus(self.link)}&size=256"
 
 
 from . import codelma_logger
