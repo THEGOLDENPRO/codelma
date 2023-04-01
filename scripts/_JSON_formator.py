3be5de853e647329633a3080fa6aa4eb34d678b1
_Wrayor <sa.adilaftabuddin@gmail.com>
2023-04-01 23:15:50 +0300

    Fixed few issues of the script and commented half of it.

--- scripts/_JSON_formator.py
+++ scripts/_JSON_formator.py
@@ -33,6 +33,8 @@ def getter(JSON_DICT):
     available_tags = ['list', 'str', 'dict', 'for', 'while', 'listcomp', 'dictcomp', 'eval',
                     'inlinestatements', 'func', 'naming', 'syntax', 'scope']    
 
+    new_dir = False
+
 # =========<USERDATA>========= #
 
     # asks user for creator name.
@@ -49,12 +51,16 @@ def getter(JSON_DICT):
         # if choice is in yes, then add the new directory.
         if choice.lower() in "yes":
             makedirs(f'quizzes\\{CREATOR}')
+            new_dir = True
+        else:
+            return None, None
 
     # ask for ID as an integer.
     while True:
 
         # find the last known ID in the creator's directory.
-        Last_ID = str(sorted(listdir(f'quizzes\\{CREATOR}\\'), key=len)[-1])[:-5]
+        if new_dir != True: Last_ID = str(sorted(listdir(f'quizzes\\{CREATOR}\\'), key=len)[-1])[:-5]
+        else: Last_ID = 0
 
         # show the last known ID for the user to
         # not make a mistake in overwriting an existing file.
@@ -67,7 +73,7 @@ def getter(JSON_DICT):
             ID = int(ID) 
             break
 
-        except ValueError: # if it returns a value error, then inform the user of the rules.
+        except ValueError: # show user: must be integer error and ask for input again.
             print("\nThe ID can only be an integer.")
             ID = int(input("\nRe-enter the ID of the question :: "))
 
@@ -75,7 +81,7 @@ def getter(JSON_DICT):
 # =========<OMIT_CODE>========= #
 
     # ask the user if they want to exclude code from the question.
-    omit_code = input('\n' + "omit_code [true|false]" + ' :: ').title()
+    omit_code = input('\nomit_code [true|false] :: ').title()
 
     # if the user enters anything except 'true' then omit_code = False, else it is True
     if omit_code not in 'True':
@@ -95,75 +101,113 @@ def getter(JSON_DICT):
     # ask the user if they want to have tags for the question. 
     if input("\nDo you want to enable tags? [y|n] :: ").lower() not in 'yes':
         
-        try: # if yes then, 
+        try: # if not yes then, remove the tags key and inform the user of the change.
             JSON_DICT.pop("tags")
             print("<<< Tags set to : Disabled >>>")
         except Exception: print("'tags' field not available.")
+        
     else: # get tags from the user.
-        tags = []
+        tags = [] # a temporary tags list
         
         while True:
-            try:
-                total = int(input('\n' + "Total tags {int}:: "))
-                break
-            except ValueError:
+            try: # ask user for the total number of tags needed.
+                total = int(input('\nTotal tags {int} :: '))
+                
+                # check if the total number of tags in not more than the total number of available_tags
+                if total < len(available_tags)+1:
+                    break
+                else: # else inform user that they should not be greedy.
+                    total = input("Total number of tags exceeds the\nnumber of available tags: Try again! :: ")
+            except ValueError: # show user: must be integer error.
                 print("Sorry you can only have integers as total number!")
+                total = int(input('\nTry Again : Total tags {int} :: '))
                 
-        print("\nValid Tags: " + str(available_tags)[1:-1])
-                
+        # show user: valid number of tags available.
+        print(f"\nValid Tags: {str(available_tags)[1:-1]}")
+
+        # take tag input
         for tagnum in range(total):
             tag = input(f'tag {tagnum + 1} :: ')   
+
+            # if tag not in valid tags: then show and error and give user another try.
             while tag not in available_tags:
                 print("\nSorry that tag is invalid Try Again")
                 tag = input(f'tag {tagnum + 1} :: ') 
+            # else add it to the temporary tags list.
             else:
                 tags.append(tag)
+
+        # modify the value in the dictionary.   
         JSON_DICT["tags"] = tags
+
+        # inform the user of the change.
         print("<<< Tags set to : Enabled >>>")
 
 # ===========<TYPE>=========== #
 
+    # Show available types.
     print("\nType:")
         
     for num, Type in enumerate(types, start=1):
         print(f'[{num}] {Type}')
 
-    selector = 5
-    
-    while selector not in [1,2,3]:
+    # add some random value for selector: -1 is nice.
+    selector = -1
+
+    # ask user for selecting a type, until type is one of the options.
+    while selector not in tuple(range(1,len(types)+1)):
         try:
-            selector = int(input('\n' + "Select Type [1|2|3]:: "))
-        except:
-            print("Sorry, you can only choose from '1', '2', '3'!")
+            selector = int(input(f"\nSelect Type :: "))
+            
+            # if the selected option doesn't exist then inform the user that they are blind.
+            if selector not in tuple(range(1,len(types)+1)):
+                print(f"Sorry, you can only choose from [{str(tuple(range(1,len(types)+1)))[1:-1]}] !")
+                
+        except ValueError: # show user: must be integer error and give another try.
+                print("Sorry you can only have integers as total number!")
+                total = int(input(f'\nTry Again : Select Type [{str(tuple(range(1,len(types)+1)))[1:-1]}] {int} :: '))
+            
     else:
+        # modify the variable in the dictionary.
         JSON_DICT["type"] = types[selector-1]
+
+        # inform the user of the change.
         print(f"<<< Type set to : {types[selector-1]} >>>")
-    
+
+    # if type is either `true_false` or `text` then remove "options" key from the dictionary.
     if JSON_DICT["type"] == 'true_false' or JSON_DICT["type"] == 'text':
         JSON_DICT.pop("options")
+
+        # inform the user of the change
         print(f"<<< Options set to : Disabled >>>")
 
 # =========<QUESTION>========= #
 
-    question = input('\n' + "Question :: ")
+    # ask useer for the question
+    question = input('\nQuestion :: ')
+
+    # modify the variable in the dictionary.
     JSON_DICT["question"] = question
 
+    # if the type is of `text`, then check if a blank ahs been provide if not inform the user of the requirement.
     while JSON_DICT["type"] == "text" and "___" not in JSON_DICT["question"].split():
         print("Sorry you missed adding a fill up blank {' ___ '} in your question\nthat is no more or less than exactly three consecutive underscores.\nMake sure there are spaces around it!")
-        JSON_DICT["question"] = input('\n' + "Question :: ")
+        # give user another try at writing the question.
+        JSON_DICT["question"] = input('\nQuestion :: ')
 
+    # inform the user of the change.
     print(f"<<< Question set to : {question} >>>")
 
 # =========<OPTIONS>========== #
 
-    if JSON_DICT["type"] == types[0]:
-        options = []
+    if JSON_DICT["type"] == types[0]: # option handling for `multiple_choice` type.
+        options = [] # temporary options list
 
-        while True:
+        while True: # ask user for total number of options.
             try:
                 total = int(input('\n' + "Total options {int} :: "))
                 break
-            except ValueError:
+            except ValueError:# show user: must be integer error.
                 print("Sorry you can only have integers as total options!")
 
         print("\nType the options carefully,\nyou don't wanna go through the hassle of editing\nbut, don't worry if you did make a mistake\nyou can re-edit through an editor\nat the end of adding all the options\n")
@@ -203,7 +247,7 @@ def getter(JSON_DICT):
     if JSON_DICT["type"] == types[0]: ############ multiple_choice ############
         answer = -1
 
-        print("\nGive the answer of this question {index}:\n" + JSON_DICT["question"])
+        print("\nGive the answer of this question {index}:\n" + f'JSON_DICT["question"]')
         for _index, _opt in enumerate(JSON_DICT["options"]):
             print(f"[{_index}] : {_opt}")
 
@@ -380,6 +424,8 @@ def json_writer(JSON_FILE, USER_DATA, JSON_DICT):
 while True:
     
     json_dict, user_data = getter(JSON_DICT)
+    if json_dict == None:
+        continue
 
     json_file = setter(json_dict)
 
